99b85f0116ceea9da1abb1b15e817f2a8ff2c957
김민태 <66211881+don72-s@users.noreply.github.com>
2024-10-25 20:06:57 +0900

    [chore] : 간단 주석 작성

--- Assets/WorkSpaces/KMT/Scripts/PlayerData.cs
+++ Assets/WorkSpaces/KMT/Scripts/PlayerData.cs
@@ -13,6 +13,8 @@ public class PlayerData
 
     public void PrintAll() {
 
+        //todo : 디버그용 코드. 후에 제거
+        //today todo : 데이터 구조 정립, 관리 데이터 인스턴스 싱글톤으로 설계
         Debug.Log(level);
         Debug.Log(exp);
 
