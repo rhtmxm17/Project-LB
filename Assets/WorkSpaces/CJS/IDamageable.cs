385a0ea5ccc66d9f0af17451b4f90eb5c5ddcb72
JeeSeong <rhtmxm17@gmail.com>
2024-10-25 14:00:05 +0900

    [Fix]: 플레이어 캐릭터의 근접공격 면역시간 제거

--- Assets/WorkSpaces/CJS/IDamageable.cs
+++ Assets/WorkSpaces/CJS/IDamageable.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 /// <summary>
 /// 공격 타입에 대한 enum입니다.
 /// 특별한 기능이 없는 일반 근접 공격이라면 DEFAULT_MELEE_ATTACK(0) 을 사용합니다.
@@ -6,4 +8,10 @@ public enum DamageType
 {
     DEFAULT_MELEE_ATTACK = 0,
     IGNORE_MELEE_IMMUNITY = 1,
+}
+
+public interface IDamageable
+{
+    public Transform transform { get; }
+    public void Damaged(int damagem, DamageType type);
 }
