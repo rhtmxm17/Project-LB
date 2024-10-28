3a9ab41b86813cbb8f367b8b2538bbf6ba96ea36
김민태 <66211881+don72-s@users.noreply.github.com>
2024-10-28 11:01:15 +0900

    [feat] : 데이터구조 정의
    간단한 데이터구조 정의

--- Assets/WorkSpaces/KMT/Scripts/PlayerData.cs
+++ Assets/WorkSpaces/KMT/Scripts/PlayerData.cs
@@ -7,20 +7,14 @@ using static StageConditionDialogueSO;
 [Serializable]
 public class PlayerData
 {
+
     public int level;
     public float exp;
-    public int[] isClearArr;
+    public int[] stageClearCntArr;
+    public bool[] hiddenArr;
+    public bool[] storyArr;
 
-    public void PrintAll() {
+    public int food;
+    public int gear;
 
-        //todo : 디버그용 코드. 후에 제거
-        //today todo : 데이터 구조 정립, 관리 데이터 인스턴스 싱글톤으로 설계
-        Debug.Log(level);
-        Debug.Log(exp);
-
-        foreach (var item in isClearArr) { 
-            Debug.Log(item);
-        }
-
-    }
 }
