5b184273fc3b91f6f18276fffb0657d4fe4ebe5c
JeeSeong <rhtmxm17@gmail.com>
2024-11-03 21:47:20 +0900

    [Feat]: 투사체 몬스터 공격부분 작성

--- Assets/Scripts/Player/ProjectileAttackMonster.cs
+++ Assets/Scripts/Player/ProjectileAttackMonster.cs
@@ -13,12 +13,16 @@ public class ProjectileMonsterAttack : MonoBehaviour
     [SerializeField] Rigidbody attackProjectilePrefab;
     [SerializeField, Tooltip("투사체가 플레이어에 닿는지 확인하는 주기")] float checkFiringLinePeriod = 0.1f; // 사선(射線) 검사 주기
     [SerializeField, Tooltip("투사체 검사에 사용될 반경")] float projectileRadius;
+    [SerializeField, Tooltip("공격 선딜레이")] float preDelay;
+    [SerializeField, Tooltip("공격 후딜레이")] float postDelay;
 
     private PlayerModel playerModel;
     private MonsterModel monsterModel;
 
     private Coroutine stateRoutine;
     private YieldInstruction sphereCastPeriod;
+    private YieldInstruction preDelayWait;
+    private YieldInstruction postDelayWait;
     private LayerMask castLayer;
 
     private void Awake()
@@ -33,6 +37,8 @@ public class ProjectileMonsterAttack : MonoBehaviour
         }
 
         sphereCastPeriod = new WaitForSeconds(checkFiringLinePeriod);
+        preDelayWait = new WaitForSeconds(preDelay);
+        postDelayWait = new WaitForSeconds(postDelay);
         castLayer = LayerMask.GetMask("Default", "Player");
     }
 
@@ -52,7 +58,26 @@ public class ProjectileMonsterAttack : MonoBehaviour
             if (Physics.SphereCast(shotPose.position, projectileRadius, playerModel.transform.position - shotPose.position, out RaycastHit hitInfo, monsterModel.DataTable.AttackRange, castLayer))
             {
                 // TODO: 캐스팅 결과 확인
+                if (hitInfo.collider.CompareTag("PlayerCollider"))
+                {
+                    StopCoroutine(stateRoutine);
+                    stateRoutine = StartCoroutine(ShotProjectile());
+                }
             }
         }
+    }
+
+    private IEnumerator ShotProjectile()
+    {
+        yield return preDelayWait;
+
+        Instantiate(attackProjectilePrefab, shotPose.position, shotPose.rotation);
+        // TODO: 투사체 초기화
+
+        yield return postDelayWait;
+
+        // 공격 완료 후 다시 추적 루틴 돌입
+        StopCoroutine(stateRoutine);
+        stateRoutine = StartCoroutine(CheckFiringLine());
     }
 }
