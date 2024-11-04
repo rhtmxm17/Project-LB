e2fbc7afb5d5c9a09a1b2cb27ecbb1d0741b2c1b
JeeSeong <rhtmxm17@gmail.com>
2024-11-04 18:13:12 +0900

    [Feat]: 게임 오버 UI에 벙커로 돌아가기 기능 연결

--- Assets/WorkSpaces/CJS/StageClearUI.cs
+++ Assets/WorkSpaces/CJS/StageClearUI.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.UI;
 
 /// <summary>
 /// UI 프리팹 내부의 필요한 참조를 전달하는 클래스
@@ -12,18 +10,6 @@ public class StageClearUI : MonoBehaviour
 {
     [SerializeField] TMP_Text foodGainValueText;
     [SerializeField] TMP_Text gearGainValueText;
-    [SerializeField] Button retryButton;
-    [SerializeField] Button returnButton;
-
-    /// <summary>
-    /// 재도전 버튼 클릭시 이벤트
-    /// </summary>
-    public UnityEvent OnRetryButtonClicked => retryButton.onClick;
-
-    /// <summary>
-    /// 벙커로 돌아가기 버튼 클릭시 이벤트
-    /// </summary>
-    public UnityEvent OnReturnButtonClicked => returnButton.onClick;
 
     /// <summary>
     /// 클리어 UI에 표시될 식량 확득량 텍스트 설정
