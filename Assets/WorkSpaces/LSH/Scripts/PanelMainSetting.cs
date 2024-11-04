860156fb65ddddbf39a029ddc05894a9f7c8f13e
tjsp9830 <mcgr1998@gmail.com>
2024-11-04 16:35:44 +0900

    [Feat]: 메인화면 세팅 제작

---
+++ Assets/WorkSpaces/LSH/Scripts/PanelMainSetting.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelMainSetting : MonoBehaviour
+{
+
+    [SerializeField] Slider slider1MouseSensitive;
+    [SerializeField] Slider slider2BGM;
+    [SerializeField] Slider slider3Voice;
+    [SerializeField] Slider slider4Effect;
+
+
+
+
+
+
+}
