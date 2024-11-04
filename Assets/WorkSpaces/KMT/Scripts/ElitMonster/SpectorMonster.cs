3990463ccdd5260f81508f63b44b4070cf88e30a
김민태 <66211881+don72-s@users.noreply.github.com>
2024-11-04 23:53:50 +0900

    [feat] : 스펙터 애니메이션
    스펙터 애니메이션과 파티클 추가

--- Assets/WorkSpaces/KMT/Scripts/ElitMonster/SpectorMonster.cs
+++ Assets/WorkSpaces/KMT/Scripts/ElitMonster/SpectorMonster.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class SpectorMonster : MonsterTakenDamage
 {
     [SerializeField] MonsterData monsterDataSO;
+    [SerializeField] ParticleSystem particleSys;
 
     protected override void MonsterDead()
     {
         if (monsterModel.MonsterCurHP <= 0)
         {
+
 
             MonsterModel instance = Instantiate(monsterDataSO.MonsterPrefab, transform.position, Quaternion.identity);
             instance.DataTable = monsterDataSO;
@@ -21,6 +23,7 @@ public class SpectorMonster : MonsterTakenDamage
 
             instance.GetComponent<MonsterTakenDamage>().OnDeadEvent += transform.parent.GetComponent<WaveManager>().CheckWaveIsClear;
 
+            particleSys.Play();
             monsterAni.SetTrigger("DeadTrigger");
             // 몬스터 사망시
             transform.SetParent(null);
